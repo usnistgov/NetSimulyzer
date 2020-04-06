a5be58cb07efe448e86e45255b9f50be74a2d7b5
Evan Black <evan.black@nist.gov>
2020-04-06 17:56:08 -0400

    Change from compat to core profile
    See: #35

--- src/main.cpp
+++ src/main.cpp
@@ -43,8 +43,8 @@ int main(int argc, char *argv[]) {
   QApplication application(argc, argv);
 
   QSurfaceFormat format;
-  format.setVersion(2, 1);
-  format.setProfile(QSurfaceFormat::CompatibilityProfile);
+  format.setVersion(3, 3);
+  format.setProfile(QSurfaceFormat::CoreProfile);
 
   QSurfaceFormat::setDefaultFormat(format);
 
