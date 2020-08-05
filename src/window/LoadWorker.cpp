5151bacddb83584e7a882e4ba36dc044cf099b37
Evan Black <evan.black@nist.gov>
2020-08-05 01:59:29 -0400

    Add loading time to scenario loaded message
    See: #90

--- src/window/LoadWorker.cpp
+++ src/window/LoadWorker.cpp
@@ -1,11 +1,18 @@
 #include "LoadWorker.h"
+#include <chrono>
 
 namespace visualization {
 
 void LoadWorker::load(const QString &fileName) {
+  using namespace std::chrono;
+
   parser.reset();
+
+  auto start = steady_clock::now();
   parser.parse(fileName.toStdString().c_str());
-  emit fileLoaded(fileName);
+  auto end = steady_clock::now();
+
+  emit fileLoaded(fileName, duration_cast<milliseconds, long>(end - start).count());
 }
 parser::FileParser &LoadWorker::getParser() {
   return parser;
